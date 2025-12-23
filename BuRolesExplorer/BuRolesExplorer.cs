3d526fc260331d37e8a40746103d989fb8abc3e3
Ivan Ficko <ivan.ficko@outlook.com>
2025-12-23 19:13:26 +0100

    Add project files.

---
+++ BuRolesExplorer/BuRolesExplorer.cs
@@ -0,0 +1,226 @@
+﻿using McTools.Xrm.Connection;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+
+namespace BuRolesExplorer
+{
+    public partial class BuRolesExplorer : PluginControlBase
+    {
+        private Settings mySettings;
+
+        private readonly Timer _searchTimer = new Timer { Interval = 300 };
+
+        public List<Entity> Users;
+        public List<Entity> FilteredUsers;
+        public Entity SelectedUser;
+
+        public List<Entity> UserRoles;
+
+        public BuRolesExplorer()
+        {
+            InitializeComponent();
+
+            _searchTimer.Tick += SearchTimer_Tick;
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+            ShowInfoNotification("This is a notification that can lead to XrmToolBox repository", new Uri("https://github.com/MscrmTools/XrmToolBox"));
+
+            // Loads or creates the settings for the plugin
+            if (!SettingsManager.Instance.TryLoad(GetType(), out mySettings))
+            {
+                mySettings = new Settings();
+
+                LogWarning("Settings not found => a new settings file has been created!");
+            }
+            else
+            {
+                LogInfo("Settings found and loaded");
+            }
+        }
+
+        private void tsbClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        /// <summary>
+        /// This event occurs when the plugin is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyPluginControl_OnCloseTool(object sender, EventArgs e)
+        {
+            // Before leaving, save the settings
+            SettingsManager.Instance.Save(GetType(), mySettings);
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (mySettings == null || detail == null) { return; }
+            mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
+            LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+        }
+
+        private void BuRolesExplorer_ConnectionUpdated(object sender, ConnectionUpdatedEventArgs e)
+        {
+            ExecuteMethod(LoadUsers);
+        }
+
+        private void LoadUsers()
+        {
+            WorkAsync(new WorkAsyncInfo("Loading users...",
+                (eventargs) =>
+                {
+                    var qe = new QueryExpression("systemuser");
+                    qe.ColumnSet = new ColumnSet("fullname");
+                    qe.AddOrder("fullname", OrderType.Ascending);
+
+                    eventargs.Result = Service.RetrieveMultiple(qe).Entities.ToList();
+                    Users = eventargs.Result as List<Entity>;
+                    FilteredUsers = new List<Entity>(Users);
+                })
+            {
+                PostWorkCallBack = (completedargs) =>
+                {
+                    if (completedargs.Error != null)
+                    {
+                        ShowErrorDialog(completedargs.Error);
+                    }
+                    else
+                    {
+                        var entities = completedargs.Result as List<Entity>;
+
+                        lbUsers.Items.Clear();
+
+                        if (entities == null) { return; }
+
+                        foreach (var entity in entities)
+                        {
+                            lbUsers.Items.Add(entity.GetAttributeValue<string>("fullname"));
+                        }
+                    }
+                }
+            });
+        }
+
+        private void SearchTimer_Tick(object sender, EventArgs e)
+        {
+            _searchTimer.Stop();
+
+            var searchText = tbUserSearch.Text.Trim().ToLowerInvariant();
+
+            FilteredUsers = string.IsNullOrEmpty(searchText)
+                ? Users
+                : Users.Where(u =>
+                    u.GetAttributeValue<string>("fullname")?
+                        .ToLowerInvariant()
+                        .Contains(searchText) == true
+                ).ToList();
+
+            lbUsers.BeginUpdate();
+            lbUsers.Items.Clear();
+
+            foreach (var user in FilteredUsers)
+            {
+                lbUsers.Items.Add(user.GetAttributeValue<string>("fullname"));
+            }
+
+            lbUsers.EndUpdate();
+        }
+
+
+        private void tbUserSearch_TextChanged(object sender, EventArgs e)
+        {
+            _searchTimer.Stop();
+            _searchTimer.Start();
+        }
+
+        private void lbUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // get selected user
+            var selectedIndex = lbUsers.SelectedIndex;
+            if (selectedIndex < 0 || selectedIndex >= Users.Count) { return; }
+
+            SelectedUser = FilteredUsers[selectedIndex];
+
+            ExecuteMethod(LoadUserRoles);
+        }
+
+        private void LoadUserRoles()
+        {
+            WorkAsync(new WorkAsyncInfo("Loading roles...",
+                (eventargs) =>
+                {
+                    var fetch = $@"
+                    <fetch>
+                      <entity name='role'>
+                        <attribute name='businessunitid' />
+                        <attribute name='name' />
+                        <link-entity name='systemuserroles' from='roleid' to='roleid' alias='S' intersect='true'>
+                          <filter>
+                            <condition attribute='systemuserid' operator='eq' value='{SelectedUser.Id:D}' />
+                          </filter>
+                        </link-entity>
+                      </entity>
+                    </fetch>";
+
+                    eventargs.Result = Service.RetrieveMultiple(new FetchExpression(fetch)).Entities.ToList();
+                })
+            {
+                PostWorkCallBack = (completedargs) =>
+                {
+                    if (completedargs.Error != null)
+                    {
+                        ShowErrorDialog(completedargs.Error);
+                    }
+                    else
+                    {
+                        var roles = completedargs.Result as List<Entity>;
+
+                        if (roles == null)
+                        {
+                            return;
+                        }
+
+                        // populate dgvUserRoles
+                        dgvUserRoles.Rows.Clear();
+                        foreach (var role in roles)
+                        {
+                            var roleName = role.GetAttributeValue<string>("name");
+                            var businessUnit = role.GetAttributeValue<EntityReference>("businessunitid")?.Name ?? "N/A";
+                            dgvUserRoles.Rows.Add(roleName, businessUnit);
+                        }
+
+                        if (dgvUserRoles.Rows.Count > 0)
+                        {
+                            dgvUserRoles.Rows[0].Selected = true;
+                        }
+
+                        UserRoles = roles;
+                    }
+                }
+            });
+        }
+
+        private void dgvUserRoles_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // select whole row on cell click
+            if (e.RowIndex >= 0)
+            {
+                dgvUserRoles.Rows[e.RowIndex].Selected = true;
+            }
+        }
+    }
+}
---
+++ BuRolesExplorer/BuRolesExplorer.designer.cs
@@ -0,0 +1,208 @@
+﻿namespace BuRolesExplorer
+{
+    partial class BuRolesExplorer
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbClose = new System.Windows.Forms.ToolStripButton();
+            this.tssSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbSample = new System.Windows.Forms.ToolStripButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbUsers = new System.Windows.Forms.ListBox();
+            this.tbUserSearch = new System.Windows.Forms.TextBox();
+            this.dgvUserRoles = new System.Windows.Forms.DataGridView();
+            this.RoleName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RoleBu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStripMenu.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserRoles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbClose,
+            this.tssSeparator1,
+            this.tsbSample});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Size = new System.Drawing.Size(1524, 34);
+            this.toolStripMenu.TabIndex = 4;
+            this.toolStripMenu.Text = "toolStrip1";
+            // 
+            // tsbClose
+            // 
+            this.tsbClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbClose.Name = "tsbClose";
+            this.tsbClose.Size = new System.Drawing.Size(129, 29);
+            this.tsbClose.Text = "Close this tool";
+            this.tsbClose.Click += new System.EventHandler(this.tsbClose_Click);
+            // 
+            // tssSeparator1
+            // 
+            this.tssSeparator1.Name = "tssSeparator1";
+            this.tssSeparator1.Size = new System.Drawing.Size(6, 34);
+            // 
+            // tsbSample
+            // 
+            this.tsbSample.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbSample.Name = "tsbSample";
+            this.tsbSample.Size = new System.Drawing.Size(68, 29);
+            this.tsbSample.Text = "Try me";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.66F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dgvUserRoles, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 37);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1518, 806);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.lbUsers, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tbUserSearch, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(500, 800);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // lbUsers
+            // 
+            this.lbUsers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbUsers.FormattingEnabled = true;
+            this.lbUsers.ItemHeight = 20;
+            this.lbUsers.Location = new System.Drawing.Point(3, 43);
+            this.lbUsers.Name = "lbUsers";
+            this.lbUsers.Size = new System.Drawing.Size(494, 744);
+            this.lbUsers.TabIndex = 0;
+            this.lbUsers.SelectedIndexChanged += new System.EventHandler(this.lbUsers_SelectedIndexChanged);
+            // 
+            // tbUserSearch
+            // 
+            this.tbUserSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbUserSearch.Location = new System.Drawing.Point(3, 3);
+            this.tbUserSearch.Name = "tbUserSearch";
+            this.tbUserSearch.Size = new System.Drawing.Size(494, 26);
+            this.tbUserSearch.TabIndex = 1;
+            this.tbUserSearch.TextChanged += new System.EventHandler(this.tbUserSearch_TextChanged);
+            // 
+            // dgvUserRoles
+            // 
+            this.dgvUserRoles.AllowUserToAddRows = false;
+            this.dgvUserRoles.AllowUserToDeleteRows = false;
+            this.dgvUserRoles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvUserRoles.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvUserRoles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUserRoles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.RoleName,
+            this.RoleBu});
+            this.dgvUserRoles.Location = new System.Drawing.Point(509, 3);
+            this.dgvUserRoles.Name = "dgvUserRoles";
+            this.dgvUserRoles.ReadOnly = true;
+            this.dgvUserRoles.RowHeadersWidth = 62;
+            this.dgvUserRoles.RowTemplate.Height = 28;
+            this.dgvUserRoles.Size = new System.Drawing.Size(1006, 800);
+            this.dgvUserRoles.TabIndex = 2;
+            this.dgvUserRoles.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUserRoles_CellClick);
+            // 
+            // RoleName
+            // 
+            this.RoleName.HeaderText = "Name";
+            this.RoleName.MinimumWidth = 8;
+            this.RoleName.Name = "RoleName";
+            this.RoleName.ReadOnly = true;
+            // 
+            // RoleBu
+            // 
+            this.RoleBu.HeaderText = "Business Unit";
+            this.RoleBu.MinimumWidth = 8;
+            this.RoleBu.Name = "RoleBu";
+            this.RoleBu.ReadOnly = true;
+            // 
+            // BuRolesExplorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.toolStripMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "BuRolesExplorer";
+            this.Size = new System.Drawing.Size(1524, 846);
+            this.ConnectionUpdated += new XrmToolBox.Extensibility.PluginControlBase.ConnectionUpdatedHandler(this.BuRolesExplorer_ConnectionUpdated);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserRoles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripButton tsbSample;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListBox lbUsers;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox tbUserSearch;
+        private System.Windows.Forms.DataGridView dgvUserRoles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RoleName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RoleBu;
+    }
+}
